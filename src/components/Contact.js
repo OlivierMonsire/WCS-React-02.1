5f780fe9bcd5980393575654ba7031214153ef5f
Olivier <olivier.monsire@gmail.com>
2020-09-21 14:53:56 +0200

    complete quest 3

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,15 +1,15 @@
 import React from "react";
 
-const Contact = () => {
+const Contact = (props) => {
     let online = 1
 
     return <div className="Contact">
-        <img className=" avatar" src="https://randomuser.me/api/portraits/men/24.jpg"  alt="image"/>
+        <img className=" avatar" src={props.avatar}  alt={props.name} />
         <div>
-            <h4 className="name" >Derrick Simpson</h4>
+            <h4 className="name" >{props.name}</h4>
             <div className="status">
-                <span className={online === 1 ? "status-online" : "status-offline"} />
-                <p className="status-text">{online === 1 ? "online" : "offline"} </p>
+                <span className={props.isOnline === true ? "status-online" : "status-offline"} />
+                <p className="status-text">{props.isOnline === true ? "online" : "offline"} </p>
             </div>
         </div>
     </div>
